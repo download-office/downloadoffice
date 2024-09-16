f978ee4256d9b1ca1765960960fdde48e477069a
download-office <conexaobolota@gmail.com>
2024-09-16 20:40:03 -0300

    Update git.php

--- git.php
+++ git.php
@@ -5,13 +5,33 @@ $fileUrl = 'https://github.com/download-office/downloadoffice/OBS_StudioV30.2.30
 // Nome sugerido para o arquivo baixado
 $fileName = 'OBS_StudioV30.2.30.rar';
 
-// Definindo cabeçalhos para forçar o download
+// Inicializa o cURL
+$ch = curl_init($fileUrl);
+
+// Configurações do cURL
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Seguir redirecionamentos, se houver
+curl_setopt($ch, CURLOPT_FAILONERROR, true);    // Reportar erro em caso de status HTTP 4xx ou 5xx
+
+// Executa a requisição
+$fileData = curl_exec($ch);
+
+// Verifica se houve erro
+if ($fileData === false) {
+    echo 'Erro ao tentar baixar o arquivo: ' . curl_error($ch);
+    exit;
+}
+
+// Fecha a sessão do cURL
+curl_close($ch);
+
+// Definindo os cabeçalhos para forçar o download
 header('Content-Type: application/octet-stream');
 header('Content-Disposition: attachment; filename="' . $fileName . '"');
-header('Content-Length: ' . filesize($fileUrl));
+header('Content-Length: ' . strlen($fileData));
 
-// Lendo o conteúdo do arquivo
-readfile($fileUrl);
+// Envia o conteúdo do arquivo para o navegador
+echo $fileData;
 
 exit;
 ?>
